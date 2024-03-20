e4bb037af55c368de951eefa16b99b53f6fb471e
lara-c7 <156355249+lara-c7@users.noreply.github.com>
2024-03-21 00:38:59 +0200

    Need_for_speed_iii.py
    Need for speed - description
    
    On the first line of the standard input, you will receive an integer n – the number of cars that you can obtain. On the next n lines, the cars themselves will follow with their mileage and fuel available, separated by "|" in the following format:
    "{car}|{mileage}|{fuel}"
    Then, you will be receiving different commands, each on a new line, separated by " : ", until the "Stop" command is given:
    "Drive : {car} : {distance} : {fuel}":
    You need to drive the given distance, and you will need the given fuel to do that. If the car doesn't have enough fuel, print: "Not enough fuel to make that ride"
    If the car has the required fuel available in the tank, increase its mileage with the given distance, decrease its fuel with the given fuel, and print: 
    "{car} driven for {distance} kilometers. {fuel} liters of fuel consumed."
    You like driving new cars only, so if a car's mileage reaches 100 000 km, remove it from the collection(s) and print: "Time to sell the {car}!"
    "Refuel : {car} : {fuel}":
    Refill the tank of your car. 
    Each tank can hold a maximum of 75 liters of fuel, so if the given amount of fuel is more than you can fit in the tank, take only what is required to fill it up. 
    Print a message in the following format: "{car} refueled with {fuel} liters"
    "Revert : {car} : {kilometers}":
    Decrease the mileage of the given car with the given kilometers and print the kilometers you have decreased it with in the following format:
    "{car} mileage decreased by {amount reverted} kilometers"
    If the mileage becomes less than 10 000km after it is decreased, just set it to 10 000km and 
    DO NOT print anything.
    Upon receiving the "Stop" command, you need to print all cars in your possession in the following format:
    "{car} -> Mileage: {mileage} kms, Fuel in the tank: {fuel} lt."
    Input / Constraints
    The mileage and fuel of the cars will be valid, 32-bit integers, and will never be negative.
    The fuel and distance amounts in the commands will never be negative.
    The car names in the commands will always be valid cars in your possession.
    Output
    All the output messages with the appropriate formats are described in the problem description.

---
+++ Exam Tasks/need_for_speed_iii.py
@@ -0,0 +1,45 @@
+number_of_cars = int(input())
+car_dict = {}
+
+for _ in range(number_of_cars):
+    car, mileage, fuel = input().split('|')
+    car_dict[car] = {'mileage': int(mileage), 'fuel': int(fuel)}
+
+while True:
+    tokens = input().split(' : ')
+    if tokens[0] == 'Stop':
+        break
+    action = tokens[0]
+    car = tokens[1]
+    if action == 'Drive':
+        distance = int(tokens[2])
+        fuel = int(tokens[3])
+        if car_dict[car]['fuel'] >= fuel:
+            car_dict[car]['mileage'] += distance
+            car_dict[car]['fuel'] -= fuel
+            print(f"{car} driven for {distance} kilometers. {fuel} liters of fuel consumed.")
+            if car_dict[car]['mileage'] >= 100000:
+                removed_car = car_dict.pop(car)
+                print(f"Time to sell the {car}!")
+        else:
+            print("Not enough fuel to make that ride")
+    elif action == 'Refuel':
+        fuel = int(tokens[2])
+        initial_fuel = car_dict[car]['fuel']
+        car_dict[car]['fuel'] += fuel
+        if car_dict[car]['fuel'] > 75:
+            car_dict[car]['fuel'] = 75
+            refueled = 75 - initial_fuel
+            print(f"{car} refueled with {refueled} liters")
+        else:
+            print(f"{car} refueled with {fuel} liters")
+    else:
+        km = int(tokens[2])
+        car_dict[car]['mileage'] -= km
+        if car_dict[car]['mileage'] < 10000:
+            car_dict[car]['mileage'] = 10000
+        else:
+            print(f"{car} mileage decreased by {km} kilometers")
+
+for car, car_info in car_dict.items():
+    print(f"{car} -> Mileage: {car_info['mileage']} kms, Fuel in the tank: {car_info['fuel']} lt.")
