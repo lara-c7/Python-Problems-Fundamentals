077b5678299cfd9fad263bb6df227aedf8c1000f
lara-c7 <156355249+lara-c7@users.noreply.github.com>
2024-03-21 22:42:57 +0200

    Create fancy_barcodes.py

---
+++ Exam Tasks/fancy_barcodes.py
@@ -0,0 +1,16 @@
+import re
+
+n = int(input())
+
+pattern = r'@#+[A-Z][A-Za-z0-9]{4,}[A-Z]@#+'
+
+for _ in range(n):
+    line = input()
+    match = re.fullmatch(pattern, line)
+    if match:
+        barcode = match.group()
+        digits = re.findall(r'\d', barcode)
+        product_group = ''.join(digits) if digits else '00'
+        print(f'Product group: {product_group}')
+    else:
+        print("Invalid barcode")
